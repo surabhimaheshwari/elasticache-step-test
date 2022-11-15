4bd36fcc43b3b64e4eccbc5016df90e1a925ff28
Surabhi Maheshwari <30389878+surabhimaheshwari@users.noreply.github.com>
2022-11-15 16:13:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-variable "access_key" {
-  type = string
-}
-variable "secret_key" {
-  type = string
-}
-
 provider "aws" {
   region = "us-west-2" 
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
 }
